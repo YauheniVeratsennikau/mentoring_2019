a6fcf01843f1ac1b0c307c08b5ed1f85770e4f67
Yauheni_Veratsennikau <yauheni_veratsennikau@epam.com>
2019-10-14 00:30:28 +0300

    [Module1] Task4

--- Module1/01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
+++ Module1/01.multithreading/MultiThreading.Task4.Threads.Join/Program.cs
@@ -13,8 +13,12 @@ using System;
 
 namespace MultiThreading.Task4.Threads.Join
 {
+    using System.Threading;
+
     class Program
     {
+        private static int queueSize = 10;
+
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -26,9 +30,52 @@ namespace MultiThreading.Task4.Threads.Join
 
             Console.WriteLine();
 
-            // feel free to add your code
-
+            ProcessReqursiveThreads(queueSize);
+            ProcessReqursiveThreadPool(queueSize);
             Console.ReadLine();
+        }
+
+        private static void ProcessReqursiveThreads(int queueIndex)
+        {
+            var thread = new Thread(() => StartReqursiveThread(queueIndex));
+            thread.Start();
+            thread.Join();
+        }
+
+        private static void StartReqursiveThread(int queueIndex)
+        {
+            queueIndex--;
+            Console.WriteLine($"Current index of thread: {queueIndex}");
+            if (queueIndex > 0)
+            {
+                ProcessReqursiveThreads(queueIndex);
+            }
+        }
+
+        private static Semaphore Sync = new Semaphore(3,3);
+
+        private static void ProcessReqursiveThreadPool(int queueIndex)
+        {
+            ThreadPool.QueueUserWorkItem(ThreadSyncWithSemaphore, queueIndex);
+        }
+
+        private static void ThreadSyncWithSemaphore(object oQueueIndex)
+        {
+            if (int.TryParse(oQueueIndex?.ToString(), out var queueIndex))
+            {
+                queueIndex--;
+                Console.WriteLine($"wants to access: {queueIndex}");
+                Sync.WaitOne(1000);
+                Console.WriteLine($"Current index of thread: {queueIndex}");
+                if (queueIndex > 0)
+                {
+                    Thread.Sleep(3000);
+                    ProcessReqursiveThreadPool(queueIndex);
+                }
+
+                Console.WriteLine($"dispose resource: {queueIndex}");
+                Sync.Release(1);
+            }
         }
     }
 }
