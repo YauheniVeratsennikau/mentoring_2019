5f9e64c760c14cce38c08ff35d15ed440cc954a7
Yauheni_Veratsennikau <yauheni_veratsennikau@epam.com>
2019-10-18 20:01:28 +0300

    [Module1] Task 5 - Sync by Semaphore and Lock constructions

--- Module1/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ Module1/01.multithreading/MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -8,8 +8,21 @@ using System;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+
     class Program
     {
+        private static int MaxQueueCount = 100;
+        private static Semaphore Sync = new Semaphore(1, 1);
+        private static ConcurrentQueue<int> concurrentQueue = new ConcurrentQueue<int>();
+
+        private static object syncObj = new object();
+        private static List<int> sharedList = new List<int>();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +30,113 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+             SyncByShemaphore();
 
+           // SyncByLock();
+
+            Console.WriteLine("The End.");
             Console.ReadLine();
         }
+
+        #region Lock
+        private static void SyncByLock()
+        {
+            Console.WriteLine("Start processing with sync by 'lock'");
+
+            Parallel.Invoke(FillSharedList, PrintLastElementFromSharedList);
+        }
+
+        private static void FillSharedList()
+        {
+            var random = new Random();
+            for (var i = 0; i < MaxQueueCount; i++)
+            {
+                lock (syncObj)
+                {
+                    var value = random.Next(100);
+                    Console.WriteLine($"Task1. Value was added to queue: {value}");
+
+                    sharedList.Add(value);
+                }
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private static void PrintLastElementFromSharedList()
+        {
+            var printedIndex = 0;
+            while (printedIndex != MaxQueueCount)
+            {
+                lock (syncObj)
+                {
+                    if (sharedList.Any() && printedIndex != sharedList.Count)
+                    {
+                        printedIndex = sharedList.Count;
+                        Console.WriteLine($"Task2. Last element of queue: {sharedList.Last()}; current index: {printedIndex}");
+                        Thread.Sleep(150);
+                    }
+                    else
+                    {
+                       // Console.WriteLine($"Try to print the same value from queue by index: {printedIndex}");
+                        Thread.Sleep(200);
+                    }
+                }
+            }
+        }
+        #endregion lock
+
+        #region Semaphore
+        private static void SyncByShemaphore()
+        {
+            Console.WriteLine("Start processing with sync by 'semaphore'");
+            Parallel.Invoke(FillTheConcurrentQueue, PrintLastElementFromConcurrentQueue);
+            //UsingTaskForProcessing();
+        }
+
+        private static void UsingTaskForProcessing()
+        {
+            var taskToFill = new TaskFactory().StartNew(FillTheConcurrentQueue);
+            var taskToPrint = new TaskFactory().StartNew(PrintLastElementFromConcurrentQueue);
+
+            Task.WaitAll(taskToFill, taskToPrint);
+        }
+
+        private static void FillTheConcurrentQueue()
+        {
+            var random = new Random();
+            for (var i = 0; i < MaxQueueCount; i++)
+            {
+                Sync.WaitOne();
+                var value = random.Next(100);
+                Console.WriteLine($"Task1. Value was added to queue: {value}");
+                concurrentQueue.Enqueue(value);
+                Thread.Sleep(200);
+                Sync.Release(1);
+            }
+        }
+
+        private static void PrintLastElementFromConcurrentQueue()
+        {
+            var printedQueueIndex = 0;
+            while (printedQueueIndex != MaxQueueCount)
+            {
+                Sync.WaitOne(1000);
+                if (!concurrentQueue.IsEmpty && printedQueueIndex != concurrentQueue.Count)
+                {
+                    printedQueueIndex = concurrentQueue.Count;
+                    Console.WriteLine($"Task2. Last element of queue: {concurrentQueue.Last()}; current index: {printedQueueIndex}");
+                }
+                else
+                {
+                    Console.WriteLine($"Try to print the same value from queue by index: {printedQueueIndex}");
+                    Thread.Sleep(200);
+                }
+
+                Sync.Release(1);
+            }
+        }
+        #endregion Semaphore
     }
 }
+
